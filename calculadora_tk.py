cdc9aee9020d5ee3747e6a2b83c83ba18724016d
MartinRFM <martin.rodriguesfm@gmail.com>
2024-09-12 19:35:30 -0300

    3 botones nuevos y correccion de pequeños errores

--- calculadora_tk.py
+++ calculadora_tk.py
@@ -6,7 +6,7 @@ root = tkinter.Tk()
 root.title("Calculadora Científica")
 root.configure(background='white')
 root.resizable(width=False, height=False)
-root.geometry("480x568+450+90")
+root.geometry("480x660+450+90")
 
 calc = tkinter.Frame(root)
 calc.grid()
@@ -21,7 +21,8 @@ class Calc():
         self.result = False
 
     def numberEnter(self, num):
-        self.result = False
+        if self.result:
+            return 
         firstnum = txtDisplay.get()
         secondnum = str(num)
         if self.input_value:
@@ -40,6 +41,7 @@ class Calc():
         txtDisplay.insert(0, value)
         txtDisplay.config(state='readonly')
 
+
     def clear_entry(self):
         self.result = False
         self.current = "0"
@@ -52,8 +54,9 @@ class Calc():
 
     def clear_last_entry(self):
         current_text = txtDisplay.get()
-        new_text = current_text[:-1]
-        self.display(new_text)
+        if self.result:
+            self.current = "0"
+            self.result = False
 
     def operation(self, op):
         self.current = float(self.current)
@@ -75,6 +78,8 @@ class Calc():
             self.total *= self.current
         if self.op == "divide":
             self.total /= self.current
+        if self.op == "potencia":
+            self.total = self.total ** self.current
         self.input_value = True
         self.check_sum = False
         self.display(self.total)
@@ -131,14 +136,27 @@ class Calc():
         self.display(self.current)
 
     def squared(self):
-        self.result = False
+        self.result = True
         self.current = math.sqrt(float(txtDisplay.get()))
         self.display(self.current)
 
     def cos(self):
-        self.result = False
+        self.result = True
         self.current = math.cos(math.radians(float(txtDisplay.get())))
         self.display(self.current)
+
+    def power(self):
+        self.result = True
+        self.current = math.pow(float(txtDisplay.get()), 2)
+        self.display(self.current)
+
+    def potencia(self):
+        self.result = True
+        self.total = float(txtDisplay.get())
+        self.display(self.total)
+        self.op = "potencia"
+        self.input_value = True
+        self.check_sum = True
 
 
 added_value = Calc()
@@ -157,26 +175,26 @@ for j in range(2, 5):
         btn[i]["command"] = lambda x=numberpad[i]: added_value.numberEnter(x)
         i += 1
 
-btnClear = tkinter.Button(calc, text=chr(67), width=6, height=2, font=('arial', 20, 'bold'), bd=4, bg='powder blue',
+btnClear = tkinter.Button(calc, text= chr(67), width=6, height=2, font=('arial', 20, 'bold'), bd=4, bg='powder blue',
                     command=added_value.clear_entry).grid(row=1, column=0, pady=1)
 
-btnAllClear = tkinter.Button(calc, text=chr(67) + chr(69), width=6, height=2, font=('arial', 20, 'bold'), bd=4, bg='powder blue',
+btnAllClear = tkinter.Button(calc, text= chr(67) + chr(69), width=6, height=2, font=('arial', 20, 'bold'), bd=4, bg='powder blue',
                     command=added_value.all_clear_entry).grid(row=1, column=1, pady=1)
 
-btnDel = tkinter.Button(calc, text="DEL", width=6, height=2, font=('arial', 20, 'bold'), bd=4, bg='powder blue',
+btnDel = tkinter.Button(calc, text= chr(68) + chr(69) + chr(76) , width=6, height=2, font=('arial', 20, 'bold'), bd=4, bg='powder blue',
                 command=added_value.clear_last_entry).grid(row=1, column=2, pady=1)
 
 btnAdd = tkinter.Button(calc, text="+", width=6, height=2, font=('arial', 20, 'bold'), bd=4, bg='powder blue',
-                command=lambda: added_value.operation("add")).grid(row=1, column=3, pady=1)
+                command=lambda: added_value.operation("add")).grid(row=2, column=3, pady=1)
 
 btnSub = tkinter.Button(calc, text="-", width=6, height=2, font=('arial', 20, 'bold'), bd=4, bg='powder blue',
-                command=lambda: added_value.operation("sub")).grid(row=2, column=3, pady=1)
+                command=lambda: added_value.operation("sub")).grid(row=3, column=3, pady=1)
 
 btnMult = tkinter.Button(calc, text="*", width=6, height=2, font=('arial', 20, 'bold'), bd=4, bg='powder blue',
-                command=lambda: added_value.operation("multi")).grid(row=3, column=3, pady=1)
+                command=lambda: added_value.operation("multi")).grid(row=4, column=3, pady=1)
 
 btnDiv = tkinter.Button(calc, text=chr(247), width=6, height=2, font=('arial', 20, 'bold'), bd=4, bg='powder blue',
-                command=lambda: added_value.operation("divide")).grid(row=4, column=3, pady=1)
+                command=lambda: added_value.operation("divide")).grid(row=5, column=3, pady=1)
 
 btnZero = tkinter.Button(calc, text="0", width=6, height=2, font=('arial', 20, 'bold'), bd=4, bg='powder blue',
                 command=lambda: added_value.numberEnter(0)).grid(row=5, column=0, pady=1)
@@ -185,7 +203,19 @@ btnDot = tkinter.Button(calc, text=".", width=6, height=2, font=('arial', 20, 'b
                 command=lambda: added_value.numberEnter(".")).grid(row=5, column=1, pady=1)
 
 btnEquals = tkinter.Button(calc, text="=", width=6, height=2, font=('arial', 20, 'bold'), bd=4, bg='powder blue',
-                    command=added_value.sum_of_total).grid(row=5, column=2, pady=1)
+                    command=added_value.sum_of_total).grid(row=6, column=3, pady=1)
+
+btnPi = tkinter.Button(calc, text="π", width=6, height=2, font=('arial', 20, 'bold'), bd=4, bg='powder blue',
+                    command=added_value.pi).grid(row=5, column=2, pady=1)
+
+btnSqrt = tkinter.Button(calc, text="√", width=6, height=2, font=('arial', 20, 'bold'), bd=4, bg='powder blue',
+                    command=added_value.squared).grid(row=6, column=0, pady=1)
+
+btnPower = tkinter.Button(calc, text="x²", width=6, height=2, font=('arial', 20, 'bold'), bd=4, bg='powder blue',
+                    command=added_value.power).grid(row=6, column=1, pady=1)
+
+btnPotencia = tkinter.Button(calc, text="^", width=6, height=2, font=('arial', 20, 'bold'), bd=4, bg='powder blue',
+                    command=added_value.potencia).grid(row=6, column=2, pady=1)
 
 
 root.mainloop()
